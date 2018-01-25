ebe749325b4a22af8b4f6bdfdfa96a02a216c8b5
BradyGo <k.ying_zhang@163.com>
2018-01-25 16:39:25 +0800

    OPtExcel_success

--- MFC-VERSION/OPTBZ/OPTBZ/OPTBZDoc.cpp
+++ MFC-VERSION/OPTBZ/OPTBZ/OPTBZDoc.cpp
@@ -3,6 +3,15 @@
 //
 
 #include "stdafx.h"
+#include "CApplication.h"
+#include "CWorkbooks.h"
+#include "CWorkbook.h"
+#include "CWorksheets.h"
+#include "CWorksheet.h"
+#include "CRectangle.h"
+#include "CRectangles.h"
+#include "CRange.h"
+
 // SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
 // ATL 项目中进行定义，并允许与该项目共享文档代码。
 #ifndef SHARED_HANDLERS
@@ -17,6 +26,7 @@
 #include "matrix.h"
 #include "mclcppclass.h"
 #include "MainFrm.h"
+
 
 using namespace std;
 
@@ -116,6 +126,155 @@ UINT ComuThread(LPVOID pParam)
 	CMainFrame * pMain=(CMainFrame*)AfxGetApp()->m_pMainWnd;  
 	CString m_strOutput = _T("Write Exl begin......");
 	pMain->AddStrOutputDebugWnd(m_strOutput);//调用CMainFrame中的自动以函数，m_strOutput是编辑框的变量
+
+	HRESULT hr;   
+	hr = CoInitialize(NULL);     
+	if(FAILED(hr))
+	{    
+		AfxMessageBox(_T("Failed to call Coinitialize()" ));
+	}
+	
+	
+	
+	CApplication ExcelApp;
+	CWorkbooks books;
+	CWorkbook book;
+	CWorksheets sheets;
+	CWorksheet sheet;
+	CRange range;
+	LPDISPATCH lpDisp = NULL;
+
+	//创建Excel 服务器(启动Excel)
+	if(!ExcelApp.CreateDispatch(_T("Excel.Application"),NULL))
+	{
+		AfxMessageBox(_T("启动Excel服务器失败!"));
+		return -1;
+	}
+
+	/*判断当前Excel的版本*/
+	CString strExcelVersion = ExcelApp.get_Version();
+	int iStart = 0;
+	strExcelVersion = strExcelVersion.Tokenize(_T("."), iStart);
+	if (_T("11") == strExcelVersion)
+	{
+		AfxMessageBox(_T("当前Excel的版本是2003。"));
+	}
+	else if (_T("15") == strExcelVersion)
+	{
+		AfxMessageBox(_T("当前Excel的版本是2013。"));
+	}
+	else
+	{
+		AfxMessageBox(_T("当前Excel的版本是其他版本。"));
+	}
+
+	ExcelApp.put_Visible(TRUE);
+	ExcelApp.put_UserControl(FALSE);
+
+	/*得到工作簿容器*/
+	books.AttachDispatch(ExcelApp.get_Workbooks());
+
+	/*打开一个工作簿，如不存在，则新增一个工作簿*/
+	CString strBookPath = _T("C:\\tmp.xls");
+	try
+	{
+		/*打开一个工作簿*/
+		lpDisp = books.Open(strBookPath, 
+			vtMissing, vtMissing, vtMissing, vtMissing, vtMissing,
+			vtMissing, vtMissing, vtMissing, vtMissing, vtMissing, 
+			vtMissing, vtMissing, vtMissing, vtMissing);
+		book.AttachDispatch(lpDisp);
+	}
+	catch(...)
+	{
+		/*增加一个新的工作簿*/
+		lpDisp = books.Add(vtMissing);
+		book.AttachDispatch(lpDisp);
+	}
+	
+
+	/*得到工作簿中的Sheet的容器*/
+	sheets.AttachDispatch(book.get_Sheets());
+
+	/*打开一个Sheet，如不存在，就新增一个Sheet*/
+	CString strSheetName = _T("NewSheet");
+	try
+	{
+		/*打开一个已有的Sheet*/
+		lpDisp = sheets.get_Item(_variant_t(strSheetName));
+		sheet.AttachDispatch(lpDisp);
+	}
+	catch(...)
+	{
+		/*创建一个新的Sheet*/
+		lpDisp = sheets.Add(vtMissing, vtMissing, _variant_t((long)1), vtMissing);
+		sheet.AttachDispatch(lpDisp);
+		sheet.put_Name(strSheetName);
+	}
+
+//	system("pause");
+
+	/*向Sheet中写入多个单元格,规模为10*10 */
+	lpDisp = sheet.get_Range(_variant_t("A1"), _variant_t("J10"));
+	range.AttachDispatch(lpDisp);
+
+	VARTYPE vt = VT_I4; /*数组元素的类型，long*/
+	SAFEARRAYBOUND sabWrite[2]; /*用于定义数组的维数和下标的起始值*/
+	sabWrite[0].cElements = 10;
+	sabWrite[0].lLbound = 0;
+	sabWrite[1].cElements = 10;
+	sabWrite[1].lLbound = 0;
+
+	COleSafeArray olesaWrite;
+	olesaWrite.Create(vt, sizeof(sabWrite)/sizeof(SAFEARRAYBOUND), sabWrite);
+
+	/*通过指向数组的指针来对二维数组的元素进行间接赋值*/
+	long (*pArray)[2] = NULL;
+	olesaWrite.AccessData((void **)&pArray);
+	memset(pArray, 0, sabWrite[0].cElements * sabWrite[1].cElements * sizeof(long));
+
+	/*释放指向数组的指针*/
+	olesaWrite.UnaccessData();
+	pArray = NULL;
+
+	/*对二维数组的元素进行逐个赋值*/
+	long index[2] = {0, 0};
+	long lFirstLBound = 0;
+	long lFirstUBound = 0;
+	long lSecondLBound = 0;
+	long lSecondUBound = 0;
+	olesaWrite.GetLBound(1, &lFirstLBound);
+	olesaWrite.GetUBound(1, &lFirstUBound);
+	olesaWrite.GetLBound(2, &lSecondLBound);
+	olesaWrite.GetUBound(2, &lSecondUBound);
+	for (long i = lFirstLBound; i <= lFirstUBound; i++)
+	{
+		index[0] = i;
+		for (long j = lSecondLBound; j <= lSecondUBound; j++)
+		{
+			index[1] = j;
+			long lElement = i * sabWrite[1].cElements + j; 
+			olesaWrite.PutElement(index, &lElement);
+		}
+	}
+
+	/*把ColesaWritefeArray变量转换为VARIANT,并写入到Excel表格中*/
+	VARIANT varWrite = (VARIANT)olesaWrite;
+	range.put_Value2(varWrite);
+
+//	system("pause");
+
+	/*根据文件的后缀名选择保存文件的格式*/
+
+
+	/*释放资源*/
+	sheet.ReleaseDispatch();
+	sheets.ReleaseDispatch();
+	book.ReleaseDispatch();
+	books.ReleaseDispatch();
+	ExcelApp.Quit();
+	ExcelApp.ReleaseDispatch();
+
 
 	while(TRUE)
 	{
